ccb0f12c5c6cb75a218b20c776301ecad274c4f2
Rem Zolotykh <remchi@gmail.com>
2015-10-03 01:07:45 +0200

    Remove static middleware for bootstrap

--- server/server.js
+++ server/server.js
@@ -7,7 +7,6 @@ mongoose.connect('mongodb://localhost/gamefinder-dev');
 
 const app = express();
 app.use(bodyParser.json());
-app.use(express.static('/node_modules/bootstrap/dist'));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
